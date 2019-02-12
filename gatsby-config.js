b4b55182721d8658273635d7b058a45c08e70ee8
kunle <adekunle.oseni@konga.com>
2019-02-12 19:57:08 +0100

    random fixes

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `Election 2019`,
-    description: `The coming Election in 2019 is coming soon...`,
+    description: `The coming Presidential Election in 2019 .`,
     author: `Oadex_09`,
   },
   plugins: [
@@ -25,6 +25,48 @@ module.exports = {
         theme_color: `#23272B`,
         display: `minimal-ui`,
         icon: `src/images/icon.png`,
+        icons: [
+          {
+            "src": "src/images/icon-32x32.png",
+            "sizes": "32x32",
+            "type": "image/png"
+          },
+          {
+            "src": "src/images/icon-70x70.png",
+            "sizes": "70x70",
+            "type": "image/png"
+          },
+          {
+            "src": "src/images/icon.png",
+            "sizes": "96x96",
+            "type": "image/png"
+          },
+          {
+            "src": "icons/icon-144x144.png",
+            "sizes": "144x144",
+            "type": "image/png"
+          },
+          {
+            "src": "icons/icon-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png"
+          },
+          {
+            "src": "icons/icon-256x256.png",
+            "sizes": "256x256",
+            "type": "image/png"
+          },
+          {
+            "src": "icons/icon-384x384.png",
+            "sizes": "384x384",
+            "type": "image/png"
+          },
+          {
+            "src": "icons/icon-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png"
+          }
+        ]
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
